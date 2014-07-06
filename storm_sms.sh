ececcebe7b186b9f635a1608903b34c35b02897f
4ch7ung <freebsd.c@gmail.com>
2014-07-06 18:13:57 +0400

    new file:   storm_sms.sh

---
+++ storm_sms.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+work_dir="$( cd "$( dirname "$0" )" && pwd )"
+if [ -z $2 ]
+then
+	echo "Usage: storm_sms.sh <phone_number> <message>"
+	exit 1
+fi
+cd $work_dir/sock
+for device in `ls`
+do
+	$work_dir/bin/msend_sms $device "$1" "$2. $device"&
+done
